1acba611d573631ac8fac2bb8de1fe3e928da379
BentlyM <metayerbently@gmail.com>
2024-06-01 19:04:10 -0400

    converted string value to int value

--- function.js
+++ function.js
@@ -3,17 +3,21 @@ const rows = document.getElementById('rows'); // value
 const btn = document.querySelector('#btn'); // adds rows
 const clear = document.createElement('button'); // clears   items in the container
 const items = document.getElementsByClassName('grid-item'); // row cards (the actual items)
+let sum = 0;
 clear.textContent = 'Clear'; // assigns inner text to the value
 
-btn.addEventListener('click' , () => {
-    let row = rows.value;
 
-    if(rows.value < 100 && items.length < 100){
-        gridLayout(row);
-        console.log(`inputted rows ${row}`);
+btn.addEventListener('click' , () => {
+    let row = +rows.value;
+    sum += row;
+    if(sum < 100 && items.length < 100){
+        gridLayout(sum);
+        console.log(`inputted rows ${sum}`);
+        console.log(sum);
     }else{
         console.log('more then a hundred');
-        console.log(items.length);
+        console.log(`number of item cards: ${items.length}`);
+        rows.value = '';
         return;
     }
     rows.value = '';
@@ -24,10 +28,13 @@ const gridLayout = (rows) => {
 };
 
 document.body.appendChild(clear).className = 'clear';
-const trial = document.getElementsByClassName('grid-item');
-
+// const trial = document.getElementsByClassName('grid-item');
 clear.addEventListener('click', () => {
-    for (i = 0; i < trial.length; i++){
-        trial[i].remove();
-    }
+    // for (i = 0; i < trial.length; i++){
+    //     items[i].remove();
+    // }
+    container.remove();
+    rows.value = '';
+    document.body.appendChild(container);
+    document.body.appendChild(clear);
 });
