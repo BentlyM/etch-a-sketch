3c347aa738f5293344727492030ccde3661f2ed4
BentlyM <metayerbently@gmail.com>
2024-06-01 12:08:42 -0400

    checks added limits to 100

--- function.js
+++ function.js
@@ -1,18 +1,26 @@
-const container = document.querySelector('#container');
-const rows = document.getElementById('rows');
-const btn = document.querySelector('#btn');
-const clear = document.createElement('button');
-const items = document.getElementsByClassName('grid-item');
-clear.textContent = 'Clear';
+const container = document.querySelector('#container'); // wraps rows
+const rows = document.getElementById('rows'); // value
+const btn = document.querySelector('#btn'); // adds rows
+const clear = document.createElement('button'); // clears   items in the container
+const items = document.getElementsByClassName('grid-item'); // row cards (the actual items)
+clear.textContent = 'Clear'; // assigns inner text to the value
 
 btn.addEventListener('click' , () => {
     let row = rows.value;
-    gridLayout(row);
+
+    if(rows.value < 100 && items.length < 100){
+        gridLayout(row);
+        console.log(`inputted rows ${row}`);
+    }else{
+        console.log('more then a hundred');
+        console.log(items.length);
+        return;
+    }
     rows.value = '';
 });
 
 const gridLayout = (rows) => {
-    
+
 };
 
 document.body.appendChild(clear).className = 'clear';
