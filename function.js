729c30f1a1daf88932124cea2a48684d4dd64f16
BentlyM <metayerbently@gmail.com>
2024-06-01 11:44:10 -0400

    main-template

--- function.js
+++ function.js
@@ -1,1 +1,25 @@
-test
+const container = document.querySelector('#container');
+const rows = document.getElementById('rows');
+const btn = document.querySelector('#btn');
+const clear = document.createElement('button');
+const items = document.getElementsByClassName('grid-item');
+clear.textContent = 'Clear';
+
+btn.addEventListener('click' , () => {
+    let row = rows.value;
+    gridLayout(row);
+    rows.value = '';
+});
+
+const gridLayout = (rows) => {
+    
+};
+
+document.body.appendChild(clear).className = 'clear';
+const trial = document.getElementsByClassName('grid-item');
+
+clear.addEventListener('click', () => {
+    for (i = 0; i < trial.length; i++){
+        trial[i].remove();
+    }
+});
